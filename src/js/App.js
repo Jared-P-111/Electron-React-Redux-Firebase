484dc3c4a9428edf0d404eac996155e29d519933
Jared_P <jaredpisell@gmail.com>
2022-09-15 17:50:39 -0700

    Bug with react router dom

--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import HomeView from '../views/Home';
-import { HashRouter as Router, Switch, Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route path="/">
           <HomeView />
         </Route>
-      </Switch>
+      </Routes>
     </Router>
   );
 };
